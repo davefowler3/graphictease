6a5e35cc685899f09dfddbef11f78f5761dbc13f
davefowler3 <davejf3@gmail.com>
2021-08-14 21:30:14 -0400

    Retooling nav and other page elements.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ function Hero() {
     <div className="hero">
       <div className="hero-info">
         <h1>GRAPHIC TEASE APPAREL</h1>
-        <p>Stay in the loop on trends and styles with urban appeal.</p>
+        <p>Styles with urban appeal</p>
         <img src={graffbw} className="hero-img" alt="Jill with spraypaint"/>
       </div>
       <div className="socials">
